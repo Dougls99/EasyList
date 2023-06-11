0d29cdc6c116db9b15766493b0446b6fb87537b0
Douglas Pereira <dougls99@outlook.com>
2023-06-11 11:56:58 -0300

    feat: adiciona tipagem do react navigation

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+
+export type propsNavigationStack = {
+    Login: undefined
+    Subscribe: undefined
+    Home: undefined
+}
+
+export type propsStack = NativeStackNavigationProp<propsNavigationStack>;
