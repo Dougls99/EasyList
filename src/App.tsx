0e58b186d5a66b46f481776e39c92ca0bc2365b5
Douglas Pereira <dougls99@outlook.com>
2023-06-11 11:52:57 -0300

    feat: adiciona o padrao de StatusBar do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
 
 import { MainStackComponent } from './routes/routes';
 
 export default function App() {
 	return (
 		<NavigationContainer>
+			<StatusBar hidden />
 			<MainStackComponent />
 		</NavigationContainer>
 	);
