2ffb2a1e2b9c9fcd59aad1d603c31fb42fec3cac
Douglas Pereira <dougls99@outlook.com>
2023-06-23 19:09:19 -0300

    feat: adiciona o firebase e finaliza a logica

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -2,10 +2,13 @@ import { useNavigation } from '@react-navigation/native';
 import React, { useState } from 'react';
 import { View, Alert, StyleSheet, ImageBackground, } from 'react-native';
 
+import { propsStack } from '../../routes';
+import { auth } from '../../dataBase/firebase';
+
 import Input from '../../components/Input';
 import Title  from '../../components/Title'
 import Button from '../../components/Button';
-import { propsStack } from '../../routes';
+import TextButton from '../../components/TextButton';
 
 export const LoginScreen = () => {
   const [email, setEmail] = useState('');
@@ -18,11 +21,27 @@ export const LoginScreen = () => {
 
   const handleLogin = () => {
     if (validateEmail(email) && password.length >= 6 ) {
-      navigation.navigate('Home');
-    } else {
-      Alert.alert('Credenciais inválidas');
-    }
+      auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log('Usuário autenticado:', user);
+        navigation.navigate('Home');
+      })
+     } else {
+         Alert.alert('Credenciais inválidas');
+     }
   };
+
+  const handleForgotPassword = () => {
+    if (validateEmail(email)){
+      auth()
+      .sendPasswordResetEmail(email)
+      .then(() => Alert.alert("Redefinir Senha", "Enviamos um e-mail para você!"))
+    } else {
+      Alert.alert('Campo de Email incorreto');
+    }
+  }
 
   const navigation = useNavigation<propsStack>();
 
@@ -35,25 +54,34 @@ export const LoginScreen = () => {
     <ImageBackground style={styles.background} source={require('../../images/background.jpg')} />
     <View style={styles.container}>
       
-      <Title text='Easy List' />
+    <View style={styles.input}>
+        <Title text='EasyList' />
 
         <Input
           onChange={setEmail}
           placeholder="Email"
           value={email}
+          keyboard='email-address'
         />
 
-        <Input 
+         <Input 
           onChange={setPassword}
           placeholder="Senha"
           value={password}
-          secureText={true}
-        />
+          keyboard='password'
+        /> 
+    </View>
 
+    <View style={styles.buttons}>
       <Button action={handleLogin} value='Login' />
-
       <Button action={handleSubscribe} value='Registre-se' />
-      
+    </View>
+    
+    <View style={styles.textButton}>
+      <TextButton action={handleForgotPassword} value='Esqueci minha senha' />
+    </View>
+    
+
     </View>
     </>
   );
@@ -63,11 +91,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
+    padding: 10,
   },
   background: {
     flex: 1,
     resizeMode: 'cover',
+  },
+  buttons: {
+    flexDirection: 'row',
+    margin: 2,
+    justifyContent: 'space-around',
+    marginBottom: 5,
+  },
+  input: {
+    alignItems: 'center',
+  },
+  textButton: {
+    backgroundColor: ' #e04d4d',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5
   },
 });
