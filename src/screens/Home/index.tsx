3f47331dbbddb69a949666ef4eab47fc308744ce
Douglas Pereira <dougls99@outlook.com>
2023-07-26 22:23:05 -0300

    feat: mpv da Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,40 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
 import Title from '../../components/Title'
-import Card from '../../components/Card';
+import CartButton from '../../components/CartButton';
+import AdditionButton from '../../components/AdditionButton';
+import InputCard from '../../components/InputCard';
+import { useState } from 'react';
+import { propsStack } from '../../routes';
 
 export function HomeScreen() {
+
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSaveValue = () => {
+    console.log(inputValue);
+  }
+
+  const navigation = useNavigation<propsStack>();
+
+  const  handleCart = () => {
+    navigation.navigate('Subscribe');
+  }
+
   return (
     <View style={styles.container}>
 
-      <Title text='EasyList' />
+      <Title text='Monte Sua Lista' />
 
-      <Card />
+      <View style={styles.input}>
+        <Text>Digite o Produto que deseja acrescentar</Text>
+        <InputCard />
+      </View>
+
+        <View style={styles.btn}>
+          <AdditionButton press={handleSaveValue} />
+          <CartButton />
+        </View>
 
     </View>
   )
@@ -18,6 +44,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    paddingTop: 50,
+    paddingTop: 150,
+    backgroundColor: "white",
   },
+  btn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingTop: 20,
+    width: 200,
+    height: 200,
+  },
+  input: {
+    paddingTop: 90,
+    alignItems: 'center',
+  }
 });
